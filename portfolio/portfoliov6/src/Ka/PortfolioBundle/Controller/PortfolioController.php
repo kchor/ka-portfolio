e0a78ca5489e7a7d53faaa155eb187a88893d2af
Karine Chor <kchor@clever-age.com>
2014-07-16 23:54:07 +0200

    init-project - add entities
    * add Project and Technology entities
    * update controller
      * add methods for each pages
      * get data for work page
      * actions to add project and techno
    * update templates

--- portfolio/portfoliov6/src/Ka/PortfolioBundle/Controller/PortfolioController.php
+++ portfolio/portfoliov6/src/Ka/PortfolioBundle/Controller/PortfolioController.php
@@ -3,31 +3,104 @@
 namespace Ka\PortfolioBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ka\PortfolioBundle\Entity\Project;
+use Ka\PortfolioBundle\Entity\Technology;
 
 class PortfolioController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('KaPortfolioBundle:Portfolio:index.html.twig');
-    }
+  public function indexAction()
+  {
+    return $this->render('KaPortfolioBundle:Portfolio:index.html.twig');
+  }
 
-    public function workAction()
-    {
-        return $this->render('KaPortfolioBundle:Portfolio:work.html.twig');
-    }
+  public function workAction()
+  {
+    $repository = $this->getDoctrine()
+                       ->getManager()
+                       ->getRepository('KaPortfolioBundle:Project');
 
-    public function cvAction()
-    {
-        return $this->render('KaPortfolioBundle:Portfolio:cv.html.twig');
-    }
+    $projects = $repository->findAll();
 
-    public function contactAction()
-    {
-        return $this->render('KaPortfolioBundle:Portfolio:contact.html.twig');
-    }
+    return $this->render('KaPortfolioBundle:Portfolio:work.html.twig', array(
+      'projects' => $projects
+    ));
+  }
 
-    public function linkAction()
-    {
-        return $this->render('KaPortfolioBundle:Portfolio:link.html.twig');
-    }
+  public function cvAction()
+  {
+    return $this->render('KaPortfolioBundle:Portfolio:cv.html.twig');
+  }
+
+  public function contactAction()
+  {
+    return $this->render('KaPortfolioBundle:Portfolio:contact.html.twig');
+  }
+
+  public function linkAction()
+  {
+    return $this->render('KaPortfolioBundle:Portfolio:link.html.twig');
+  }
+
+  public function addProjectAction()
+  {
+    $project = new Project();
+    $project->setName('Jeu MACSF');
+    $project->setDescription('Projet en D7 bla bla');
+    $project->setShortDescription('Projet en D7');
+    $project->setDuration('Un mois');
+    $project->setLink('http://demarrermaviepro.fr');
+    $project->setPicture('macsf_picture');
+
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($project);
+
+    $em->flush();
+
+    $this->get('session')->getFlashBag()->add('info', 'Project added !');
+
+    return $this->render('KaPortfolioBundle:Portfolio:addProject.html.twig');
+  }
+
+  public function addTechnologyAction()
+  {
+    $project = new Technology();
+    $project->setName('Drupal 7');
+    $project->setCategory('Informatique');
+    $project->setPicture('info_picture');
+
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($project);
+
+    $project = new Technology();
+    $project->setName('PHP');
+    $project->setCategory('Informatique');
+    $project->setPicture('info_picture');
+    $em->persist($project);
+
+    $project = new Technology();
+    $project->setName('Drupal 7');
+    $project->setCategory('Informatique');
+    $project->setPicture('info_picture');
+    $em->persist($project);
+
+    $project = new Technology();
+    $project->setName('CSS');
+    $project->setCategory('Informatique');
+    $project->setPicture('info_picture');
+    $em->persist($project);
+
+    $project = new Technology();
+    $project->setName('MySQL');
+    $project->setCategory('Informatique');
+    $project->setPicture('info_picture');
+    $em->persist($project);
+
+    $em->flush();
+
+    $this->get('session')->getFlashBag()->add('info', 'Techno added !');
+
+    return $this->render('KaPortfolioBundle:Portfolio:addTechnology.html.twig');
+  }
+
+
 }
---
+++ portfolio/portfoliov6/src/Ka/PortfolioBundle/Entity/Technology.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Ka\PortfolioBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Technology
+ *
+ * @ORM\Table(name="technologies")
+ * @ORM\Entity(repositoryClass="Ka\PortfolioBundle\Entity\TechnologyRepository")
+ */
+class Technology
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="picture", type="string", length=255)
+     */
+    private $picture;
+
+    /**
+     * @var \stdClass
+     *
+     * @ORM\Column(name="category", type="object")
+     */
+    private $category;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Technology
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set picture
+     *
+     * @param string $picture
+     * @return Technology
+     */
+    public function setPicture($picture)
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    /**
+     * Get picture
+     *
+     * @return string
+     */
+    public function getPicture()
+    {
+        return $this->picture;
+    }
+
+    /**
+     * Set category
+     *
+     * @param \stdClass $category
+     * @return Technology
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \stdClass
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+}
